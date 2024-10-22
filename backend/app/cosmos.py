e4e867c6d5881c40ba3a6425211fc7d111990c68
pbgao <canhuaonly@163.com>
2024-10-22 09:04:45 +0700

    共通提交

--- backend/app/cosmos.py
+++ backend/app/cosmos.py
@@ -1,4 +1,5 @@
 """ Cosmos DB Utils"""
+
 from azure.cosmos.partition_key import PartitionKey
 from azure.cosmos import cosmos_client
 from azure.cosmos import exceptions
@@ -22,14 +23,43 @@ def get_database():
         database = get_client().create_database(id=config.settings.AZURE_DATABASE_ID)
         print("数据库创建成功")
         try:
-            database.create_container(id="demo_users", partition_key=PartitionKey(path="/id"))
+            database.create_container(
+                id="demo_users", partition_key=PartitionKey(path="/id")
+            )
             print("容器users创建成功")
-            database.create_container(id="demo_posts", partition_key=PartitionKey(path="/type"))
+            database.create_container(
+                id="demo_posts", partition_key=PartitionKey(path="/type")
+            )
             print("容器posts创建成功")
-            database.create_container(id="users", partition_key=PartitionKey(path="/userId"))
+            database.create_container(
+                id="users", partition_key=PartitionKey(path="/userId")
+            )
             print("容器users创建成功")
-            database.create_container(id="messages", partition_key=PartitionKey(path="/chatId"))
+            database.create_container(
+                id="messages", partition_key=PartitionKey(path="/chatId")
+            )
             print("容器messages创建成功")
+            database.create_container(
+                id="contacts", partition_key=PartitionKey(path="/user_id")
+            )
+            print("容器contacts创建成功")
+            database.create_container(
+                id="chat_user", partition_key=PartitionKey(path="/user_id")
+            )
+            print("容器chat_user创建成功")
+            database.create_container(
+                id="chat_history_file", partition_key=PartitionKey(path="/thread_id")
+            )
+            print("容器chat_history_file创建成功")
+            database.create_container(
+                id="chat_history_message", partition_key=PartitionKey(path="/thread_id")
+            )
+            print("容器chat_history_message创建成功")
+            database.create_container(
+                id="chat_history_thread",
+                partition_key=PartitionKey(path="/user_application_id"),
+            )
+            print("容器chat_history_thread创建成功")
         except exceptions.CosmosResourceExistsError:
             print("容器创建失败")
         return database
